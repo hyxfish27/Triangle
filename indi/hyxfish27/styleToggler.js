e7a432ce4203de564859652967c1800b7282b972
hyxfish27 <avicky1515@gmail.com>
2022-08-22 15:40:30 +0800

    chore: 調整專案 namespace

---
+++ indi/hyxfish27/styleToggler.js
@@ -0,0 +1,10 @@
+const styleToggler = (element) => (className) => (size) => (margin) => (symbol) => {
+    element.className = className;
+    element.style.width = size;
+    element.style.height = size;
+    element.style.margin = margin;
+    element.innerText = symbol;
+  };
+
+export default styleToggler;
+  
